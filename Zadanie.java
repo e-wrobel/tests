bba583ed0c4ce2c0b3136a0de0549e7fafd84c9d
Mr. Sparrow <wrobel@e-wrobel.pl>
2019-02-08 15:47:16 +0100

    Update Zadanie.java

--- Zadanie.java
+++ Zadanie.java
@@ -1,4 +1,6 @@
- static public void main (String[] args){
+public class Zadanie {
+
+  static public void main (String[] args){
 
 
     /*
@@ -15,35 +17,43 @@
      */
 
     int[][] input_data = {
-            {0,0,1,1,1,1,1,1,1,1,0,0,0},
-            {0,0,0,0,0,0,0,0,0,1,0,0,0},
-            {0,0,0,0,0,0,0,0,0,1,0,0,0},
-            {0,0,0,0,0,0,0,0,0,1,0,0,0},
-            {0,0,0,0,0,0,1,1,1,1,0,0,0},
-            {0,0,0,0,0,0,1,0,0,0,0,0,0},
-            {0,0,0,0,0,0,1,0,0,0,0,0,0}
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}
     };
 
     // Starting point / coordinates
-    int i = 0;
-    int j = 0;
+    int found = 0;
+    StartingPoint starting_point;
 
     int w = input_data[0].length;
     int h = input_data.length;
 
-    for (i = 0; i < h; i++){
-      for (j = 0; j < w; j++){
-        if (input_data[i][j] == 1){
-
+    for (int i = 0; i < h; i++) {
+      if (found == 0) {
+        for (int j = 0; j < w; j++) {
+          if (input_data[i][j] == 1) {
+//            starting_point.SetPoints(i, j);
+            starting_point = new StartingPoint(i, j);
+            found = 1;
+            break;
+          }
         }
+      } else {
+        break;
       }
     }
 
+    System.out.println("b");
+  }
 
-  StartingPoint starting_point;
-
-  static void recursion(int[][] input){
-
+  static void recursion(StartingPoint starting_point){
+    int x = starting_point.x;
+    int y = starting_point.y;
   }
 }
 
@@ -51,7 +61,12 @@ class StartingPoint{
   int x;
   int y;
 
-  StartingPoint(x, y){
+  StartingPoint(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public void SetPoints(int x, int y){
     this.x = x;
     this.y = y;
   }
