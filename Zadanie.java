807abd4eb1387d1249073818c7159eb81ffddd60
Marcin <wrobel@e-wrobel.pl>
2019-02-10 18:19:58 +0100

    Added Zadanie

--- Zadanie.java
+++ Zadanie.java
@@ -28,7 +28,7 @@ public class Zadanie {
 
     // Starting point / coordinates
     int found = 0;
-    StartingPoint starting_point;
+    StartingPoint starting_point = null;
 
     int w = input_data[0].length;
     int h = input_data.length;
@@ -37,7 +37,6 @@ public class Zadanie {
       if (found == 0) {
         for (int j = 0; j < w; j++) {
           if (input_data[i][j] == 1) {
-//            starting_point.SetPoints(i, j);
             starting_point = new StartingPoint(i, j);
             found = 1;
             break;
@@ -48,12 +47,86 @@ public class Zadanie {
       }
     }
 
+    walk(starting_point, input_data);
     System.out.println("b");
   }
 
-  static void recursion(StartingPoint starting_point){
-    int x = starting_point.x;
-    int y = starting_point.y;
+  static void walk(StartingPoint starting_point, int[][] input_data){
+      int x = starting_point.x;
+      int y = starting_point.y;
+
+      // Possible directions to walk through -> 0 means not possible to walk in the particular direction.
+      // direction[0] = 1 -> go right
+      // direction[1] = 1 -> go left
+      // direction[2] = 1 -> go up
+      // direction[3] = 1 -> go down
+      // directions[4] = number, and it means how many paths we can choose
+      // At the beginning we do not know about possible directions so everything = 0
+      int[] directions = {0, 0, 0, 0, 0};
+
+      // If given point is a crossroad then you can go in more than one direction
+      // So we have multiple choices, it means that we can go in multiple paths
+      directions = give_directions(starting_point, input_data);
+
+      int number_of_paths = directions[4];
+
+      if (number_of_paths > 1){
+          // for every path execute recursivly walk method!
+      }
+      else if (number_of_paths == 1){
+          // We are going into one direction
+      }
+      else {
+          // We have finished
+      }
+
+
+  }
+
+  static int[] give_directions(StartingPoint given_point, int[][] input_data){
+
+      int[] directions = {0, 0, 0, 0, 0};
+
+      // Number of possible paths, 1 means that we can go only into one direction
+      int number_of_paths = 0;
+
+      // Boundary conditions
+      int x_max = input_data.length -1;
+      int y_max = input_data[0].length -1;
+
+      // Possition in matrix
+      // x -> possition up|down
+      // y -> possition left|righ
+      int x = given_point.x;
+      int y = given_point.y;
+
+      // If we can go right
+      if (y < y_max && input_data[x][y+1] == 1){
+          directions[0] = 1;
+          number_of_paths +=1;
+      }
+
+      // if we can go left
+      if (y > 0 && input_data[x][y -1] == 1){
+          directions[1] = 1;
+          number_of_paths +=1;
+      }
+
+      // if we can go down
+      if (x < x_max && input_data[x +1][y] == 1){
+          directions[2] = 1;
+          number_of_paths +=1;
+      }
+
+      // if we can go up
+      if (x > 0 && input_data[x -1][y] == 1){
+          directions[3] = 1;
+          number_of_paths +=1;
+      }
+
+      directions[4] = number_of_paths;
+
+      return directions;
   }
 }
 
@@ -66,9 +139,9 @@ class StartingPoint{
     this.y = y;
   }
 
-  public void SetPoints(int x, int y){
-    this.x = x;
-    this.y = y;
-  }
+//  public void SetPoints(int x, int y){
+//    this.x = x;
+//    this.y = y;
+//  }
 
 }
